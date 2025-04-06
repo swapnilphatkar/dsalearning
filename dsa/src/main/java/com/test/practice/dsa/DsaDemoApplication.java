2530ed909480ce3b7949526f055205161306f5cd
swapnilphatkar <phatkar.swapnil@yahoo.com>
2025-04-06 10:57:22 +0530

    initial push for this repo

---
+++ dsa/src/main/java/com/test/practice/dsa/DsaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.test.practice.dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaDemoApplication.class, args);
+	}
+
+}
---
+++ dsa/src/main/java/com/test/practice/dsa/dsa1/TestDSA1.java
@@ -0,0 +1,8 @@
+package com.test.practice.dsa.dsa1;
+
+public class TestDSA1 {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
